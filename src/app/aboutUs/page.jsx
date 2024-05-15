603cc6b5a2ab06561afa41012adac795aead7383
siconfit <siconfitgeren@gmail.com>
2024-05-15 18:47:06 -0500

    new design about us

--- src/app/aboutUs/page.jsx
+++ src/app/aboutUs/page.jsx
@@ -1,34 +1,56 @@
 import Link from "next/link";
 
 export default function aboutUs() {
-    return (
-        <div className="max-w-7xl mx-auto">
-            <div className="grid lg:grid-cols-2 grid-cols-1 gap-10 py-16">
-                <div className="mx-4 md:mx-16 relative">
-                    <h1 className="text-5xl font-bold text-center text-black">¿Quienes somos?</h1>
-                    <p className="text-center font-bold bg-[#373739] text-[#c1a161] text-xl md:text-2xl lg:text-3xl mt-16 p-8 rounded-[25px] text-wrap ">
-                        Somos Grease Nails Spa & Alta Peluquería, llegamos a la ciudad de Ibarra a ofrecerte productos y servicios de alta calidad y que cumplan todas las expectativas que tienes, nuestros servicios son para todos hombres, mujeres y niños.
-                        En nuestra cartera de servicios te ofrecemos diseños de cejas, laminados, lifting de pestañas, pestañas pelo a pelo, manicura, pedicura, keratina, hidratación, masajes, y muchas cosas más para ti.
-                    </p>
-                </div>
-
-                <div className="relative hidden lg:block">
-                    <img src="SERVICE1.jpeg" className="rounded-lg absolute h-[250px] top-[5%] right-8" />
-                    <img src="SERVICE2.jpeg" className="rounded-lg absolute h-[200px] top-[10%] left-24" />
-                    <img src="SERVICE3.jpeg" className="rounded-lg absolute h-[200px] top-[55%] right-8" />
-                    <img src="SERVICE4.jpeg" className="rounded-lg absolute h-[200px] top-[60%] left-16" />
-                </div>
-                <div className="relative flex justify-center lg:hidden">
-                    <img src="SERVICE1.jpeg" className="rounded-lg h-[250px]" />
-                </div>
-                <div className="flex justify-center items-center">
-                    <button className=" bg-[#373739] text-[#c1a161] px-8 py-4 rounded-full relative">
-                        <Link href="/">Regresar</Link>
-                    </button>
-                </div>
-
+  return (
+    <div className="mx-auto max-w-7xl">
+      <div className="relative flex flex-col items-center justify-center py-16">
+        <h1 className="text-center text-5xl font-bold text-black sm:text-7xl">
+          ¿Quienes somos?
+        </h1>
+        <div className="mt-16">
+          <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+            <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800">
+              <img
+                src="SERVICE1.jpeg"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
             </div>
+            <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800">
+              <img
+                src="SERVICE2.jpeg"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
+            </div>
+            <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800">
+              <img
+                src="SERVICE3.jpeg"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
+            </div>
+            <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800">
+              <img
+                src="SERVICE4.jpeg"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
+            </div>
+            <div className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800">
+              <img
+                src="SERVICE5.jpeg"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
+            </div>
+          </div>
         </div>
-
-    )
+        <p className="mx-10 mt-16 max-w-3xl text-wrap rounded-[25px] bg-[#373739] p-8 text-center text-xl font-bold text-[#c1a161] shadow-xl md:text-2xl lg:mx-0 lg:text-3xl">
+          Somos Grease Nails Spa & Alta Peluquería, llegamos a la ciudad de
+          Ibarra a ofrecerte productos y servicios de alta calidad y que cumplan
+          todas las expectativas que tienes, nuestros servicios son para todos
+          hombres, mujeres y niños. En nuestra cartera de servicios te ofrecemos
+          diseños de cejas, laminados, lifting de pestañas, pestañas pelo a
+          pelo, manicura, pedicura, keratina, hidratación, masajes, y muchas
+          cosas más para ti.
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,8 +34,8 @@ export default function Header() {
             aria-hidden="true"
           >
             <path
-              clip-rule="evenodd"
-              fill-rule="evenodd"
+              clipRule="evenodd"
+              fillRule="evenodd"
               d="M2 2.75A.75.75 0 0 1 2.75 2h10.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 2.75Zm0 10.5a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75ZM2 6.25a.75.75 0 0 1 .75-.75h10.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 6.25Zm0 3.5A.75.75 0 0 1 2.75 9h10.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 9.75Z"
             ></path>
           </svg>
